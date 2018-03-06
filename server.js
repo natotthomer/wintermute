04dd7a1c963c7d5f06c29b09a7737f65422cffd4
Nathaniel Ott Homer <nathaniel.ott.homer@gmail.com>
2018-03-06 10:28:20 -0500

    update server, add start command

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ var express = require('express');
 
 const app = express();
 
-const port = 3000;
+// const port = 3000;
 
 app.use('/', express.static('src'));
 
-app.listen(port, function() {
-  console.log("Server Started at port 3000");
+const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || 'production';
+app.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  console.info(`Server running on http://localhost:${port} [${env}]`);
 });
+
+// app.listen(port, function() {
+//   console.log("Server Started at port 3000");
+// });
