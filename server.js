88ff3826f9acfbd5d4d34d630e0539bd9a193ecb
Nathaniel Ott Homer <nathaniel.ott.homer@gmail.com>
2018-04-02 16:15:25 -0400

    server can accept process.env variables for PORT and NODE_ENV

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 var express = require('express');
 
 const app = express();
-
-// const port = 3000;
+const port = process.env.PORT || 3000;
+const env = process.env.NODE_ENV || 'production';
 
 app.use('/', express.static('src'));
 
-const port = process.env.PORT || 3000;
-const env = process.env.NODE_ENV || 'production';
 app.listen(port, err => {
   if (err) {
     return console.error(err);
   }
   console.info(`Server running on http://localhost:${port} [${env}]`);
 });
-
-// app.listen(port, function() {
-//   console.log("Server Started at port 3000");
-// });
